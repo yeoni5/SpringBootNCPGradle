eca44fa7e07d73d6d0692e6429f6ef9738565826
yeoni5 <yeoni5@naver.com>
2024-05-16 14:16:13 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP Test !!</h1>
+      <img src='./image/mangsang.gif' alt='mangsang' />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main/java/com/example/demo/SpringBootNcpGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNcpGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNcpGradleApplication.class, args);
+	}
+
+}
